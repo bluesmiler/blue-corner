3f252019c4a1137e2ef367bdc6643bfd8ebe9e21
blue <for_clementine@163.com>
2023-07-05 09:11:46 +0800

    rebuild

---
+++ src/main/java/com/blue/corner/BlueCornerStarter.java
@@ -0,0 +1,11 @@
+package com.blue.corner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueCornerStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(BlueCornerStarter.class);
+    }
+}
